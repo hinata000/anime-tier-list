82797aec55a6e8b7e7e2eff40e0ac92f8890c573
hinata000 <hinata0125125@icloud.com>
2022-12-20 04:30:04 +0900

    homeを微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  def set_search
+    @query = { title_cont: params[:q] }
+    @search = Animation.ransack(@query)
+    @search_posts = @search.result.order(created_at: :desc)
+  end
 end
