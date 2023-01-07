6674c059289e4b90fa5a855f97c7835eff02f43d
hinata000 <hinata0125125@icloud.com>
2023-01-08 00:40:25 +0900

    ログイン画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   resources :animations, only: [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_072804) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_083902) do
   create_table "animation_details", charset: "utf8mb4", force: :cascade do |t|
     t.integer "animation_id"
     t.text "staffs"
@@ -32,6 +32,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_072804) do
     t.integer "syobocal_tid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
